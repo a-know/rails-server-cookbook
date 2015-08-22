bf60757e4592de3aef4992ec096070f920a7b096
a-know <a.know.3373@gmail.com>
2015-08-22 11:31:25 +0900

    Install PostgreSQL

--- site-cookbooks/postgresql/recipes/default.rb
+++ site-cookbooks/postgresql/recipes/default.rb
@@ -1,8 +1,19 @@
-#
-# Cookbook Name:: postgresql
-# Recipe:: default
-#
-# Copyright 2015, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+package "postgresql" do
+  action :install
+end
+
+package "postgresql-server" do
+  action :install
+end
+
+service "postgresql" do
+  action [:enable]
+  supports :start => true
+end
+
+execute "postgrosql-init" do
+  not_if "test -f /var/lib/pgsql/data/postgresql.conf"
+  command "service postgresql initdb"
+  action :run
+  notifies :start, 'service[postgresql]'
+end
