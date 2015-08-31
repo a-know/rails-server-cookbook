858559aa260127b42e60e63f8d34d550c35390cf
a-know <a.know.3373@gmail.com>
2015-08-31 15:13:17 +0900

    Configure nginx settings and create spec

--- site-cookbooks/nginx_settings/recipes/default.rb
+++ site-cookbooks/nginx_settings/recipes/default.rb
@@ -6,3 +6,17 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'nginx'
+
+directory "/var/www/#{node[:nginx_settings][:application_name]}" do
+  owner 'a-know'
+  action :create
+  recursive true
+end
+
+template '/etc/nginx/sites-available/default' do
+  action :create
+  source 'default.conf.erb'
+  notifies :reload, 'service[nginx]'
+end
