9427acaeb34cb5b39bb66ee8b62aae67a5dcbcad
a-know <a.know.3373@gmail.com>
2015-09-13 10:22:18 +0900

    Create recipe for change selinux policy for unicorn

--- site-cookbooks/selinux-policies/recipes/unicorn.rb
+++ site-cookbooks/selinux-policies/recipes/unicorn.rb
@@ -1,8 +1,26 @@
 #
 # Cookbook Name:: selinux-policies
-# Recipe:: default
+# Recipe:: unicorn
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
+
+unicorn_policy_file = 'unicorn.pp'
+unicorn_policy_path = File.join('/var/tmp', unicorn_policy_file)
+
+cookbook_file unicorn_policy_path do
+  user     'root'
+  group    'root'
+  mode     0600
+  source   unicorn_policy_file
+  not_if   "semodule -l | egrep \"unicorn\\s\*1.0\""
+  notifies :run, 'execute[change SELinux policy for unicorn]', :immediately
+end
+
+execute "change SELinux policy for unicorn" do
+  user "root"
+  command "semodule -i #{unicorn_policy_path}"
+  action :nothing
+end
